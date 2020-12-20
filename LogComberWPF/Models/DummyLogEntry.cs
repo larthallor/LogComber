205ff8f67b672a8b49d701b0f9ffd2ad0afca71f
Andrew Bennett <andrew@bennettsathome.com>
2020-12-20 01:16:43 -0500

    Add project files.

---
+++ LogComberWPF/Models/DummyLogEntry.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogComberWPF.Models
+{
+    public class DummyLogEntry : INotifyPropertyChanged
+    {
+        private string _fileSource = null;
+        public string FileSource
+        {
+            get
+            {
+                return _fileSource;
+            }
+            set
+            {
+                if (value != _fileSource)
+                {
+                    _fileSource = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+        //<DataGridTextColumn Header = "time" />
+        private string _time = null;
+        public string Time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                if(value != _time)
+                {
+                    _time = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        //< DataGridTextColumn Header="c-ip"/>
+        private string _cIp;
+        public string CIP
+        {
+            get { return _cIp; }
+            set 
+            {
+                if (_cIp != value)
+                {
+                    _cIp = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        //<DataGridTextColumn Header = "cs-method" />
+        private string _csMethod;
+
+        public string CsMethod
+        {
+            get { return _csMethod; }
+            set
+            {
+                if (_csMethod != value)
+                {
+                    _csMethod = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        //< DataGridTextColumn Header="cs-uri-stem"/>
+        private string _csUriStem;
+
+        public string CsUriStem
+        {
+            get { return _csUriStem; }
+            set 
+            {
+                if (_csUriStem != value)
+                {
+                    _csUriStem = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        //<DataGridTextColumn Header = "sc-status" />
+
+        private string _scStatus;
+        public string ScStatus
+        {
+            get { return _scStatus; }
+            set
+            {
+                if (_scStatus != value)
+                {
+                    _scStatus = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        //< DataGridTextColumn Header="cs-version"/>
+        private string _csVersion;
+
+        public string CsVersion
+        {
+            get { return _csVersion; }
+            set
+            {
+                if (_csVersion != value)
+                {
+                    _csVersion = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ LogComberWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿using LogComberWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogComberWPF.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<DummyLogEntry> _dummyLogEntries = new();
+
+        public ObservableCollection<DummyLogEntry> DummyLogEntries 
+        { 
+            get
+            {
+                return _dummyLogEntries;
+            }
+            set
+            {
+                if(_dummyLogEntries != value)
+                {
+                    _dummyLogEntries = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        } 
+
+        public MainWindowViewModel()
+        {
+            DummyLogEntries.Add(
+                new DummyLogEntry()
+                {
+                    //17:42:15 172.16.255.255 GET /default.htm 200 HTTP/1.0 
+                    FileSource = @"C:\Users\abennett\Downloads\U12346.log",
+                    Time = "17:42:15",
+                    CIP = "172.16.255.255",
+                    CsMethod = "GET",
+                    CsUriStem = "/default.htm",
+                    ScStatus = "200",
+                    CsVersion = "HTTP/1.0"
+                }
+                ) ;
+
+            DummyLogEntries.Add(
+                new DummyLogEntry()
+                {
+                    //17:42:15 172.16.255.255 GET /default.htm 200 HTTP/1.0
+                    FileSource = @"C:\Users\abennett\Downloads\U12346.log",
+                    Time = "17:42:16",
+                    CIP = "172.16.255.255",
+                    CsMethod = "GET",
+                    CsUriStem = "/images/flower.jpg",
+                    ScStatus = "200",
+                    CsVersion = "HTTP/1.0"
+                }
+                );
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
