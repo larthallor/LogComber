bdfdbf402ba610e8f06f85ae16323d46efed5547
Andrew Bennett <andrew@bennettsathome.com>
2022-01-23 10:36:48 -0500

    Refactored non-UI into separate class library. Changed parser methods to all static.

---
+++ W3CLogFileLibrary/W3CLogParser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace W3CLogFileLibrary;
+
+public class W3CLogParser
+{
+    public static async IAsyncEnumerable<W3CRecord> GetRecordsAsync(string filename)
+    {
+        var defaultDate = DateTime.UtcNow.Date;
+
+        string?[]? fieldNames = null;
+
+        using var sr = File.OpenText(filename);
+        string? lineBuffer = null;
+        while (!sr.EndOfStream)
+        {
+            lineBuffer = await sr.ReadLineAsync();
+            if (lineBuffer == null) {}
+            else if (lineBuffer.StartsWith("#Version:")) {}
+            else if (lineBuffer.StartsWith("#Date:"))
+            {
+                defaultDate = ParseDateDirective(lineBuffer);
+            }
+            else if (lineBuffer.StartsWith("#Fields:"))
+            {
+                fieldNames = ParseFieldsDirective(lineBuffer);
+            }
+            else if (lineBuffer.StartsWith("#")) { }
+            else
+            {
+
+                // if we get here, this is data, not a directive.
+                if (fieldNames == null) { continue; } // If we don't have a fieldNames directive yet.
+                W3CRecord record = new();
+                var fields = lineBuffer.Split(' ');
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    record = SetData(fields[i], fieldNames[i], record, defaultDate);
+                }
+                yield return record;
+            }
+        }
+
+        sr.Close();
+    }
+
+    private static string[] ParseFieldsDirective(string lineBuffer)
+    {
+        var tokens = lineBuffer.Split(' ');
+        return tokens[1..];
+    }
+
+    private static DateTime ParseDateDirective(string lineBuffer)
+    {
+        var tokens = lineBuffer.Split(' ');
+        return DateTime.Parse(tokens[1]);
+    }
+
+    private static W3CRecord SetData(string rawField, string? rawFieldName, W3CRecord record, DateTime defaultDateTime)
+    {
+        return rawFieldName switch
+        {
+            "time" => record with
+            {
+                EventTimeUTC = defaultDateTime.Add(TimeSpan.Parse(rawField))
+            },
+            "cs-method" => record with { CS_Method = rawField },
+            "cs-uri" => record with { CS_URI_Stem = rawField },
+            _ => record,
+        };
+    }
+}
---
+++ W3CLogFileLibrary/W3CRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace W3CLogFileLibrary;
+
+public record W3CRecord
+{
+    public string? LogFilename { get; init; }
+    public DateTime? EventTimeUTC { get; init; }
+    public DateTime? EventTimeLocal
+    {
+        get
+        {
+            return EventTimeUTC?.ToLocalTime();
+        }
+    }
+    public string? S_SiteName { get; init; }
+    public string? S_ComputerName { get; init; }
+    public string? S_Ip { get; init; }
+    public string? CS_Method { get; init; }
+    public string? CS_URI_Stem { get; init; }
+    public string? CS_URI_Query { get; init; }
+    public string? S_Port { get; init; }
+    public string? CS_Username { get; init; }
+    public string? C_IP { get; set; }
+    public string? CS_Version { get; init; }
+    public string? CS_UserAgent { get; init; }
+    public string? CS_Cookie { get; init; }
+    public string? CS_Referrer { get; init; }
+    public string? CS_Host { get; init; }
+    public string? SC_Status { get; init; }
+    public string? SC_SubStatus { get; init; }
+    public string? SC_Win32_Status { get; init; }
+    public long? SC_Bytes { get; init; }
+    public long? CS_Bytes { get; init; }
+    public int? TimeTaken { get; init; }
+    public string? StreamId { get; init; }
+}
